4e1aeb6ea88a06efe0b44230ca74d093a6705292
丁 坚 <djqq1987@gmail.com>
2015-05-02 22:11:00 +0800

    bugfix

--- app.js
+++ app.js
@@ -5,25 +5,26 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes');
 
 var app = express();
 
+global.openValue = 0;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,18 +34,6 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
 
 // production error handler
 // no stacktraces leaked to user
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var indexMod = require('../models/index');
+var secret = require('../secret');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/t', function(req, res, next) {
+
+  var ip=req.headers['x-real-ip'] || req.headers['x-forwarded-for']||req.ip;
+  var createDate = new Date();
+  var ua = req.headers["user-agent"];
+  var referer = req.headers["referer"];
+  res.setHeader("Content-Type", "application/javascript");
+  var obj = {
+    ip:ip,createDate:createDate,ua:ua,referer:referer
+  };
+  indexMod.findIp(ip,function(err,result){
+    indexMod.insert(obj,function(err,result){
+      if(err){
+        console.log("error:"+ip);
+      }
+    })
+    if(err){
+      return next(err);
+    }
+    if(global.openValue <=0){
+      res.render("index");
+    }else{
+      global.openValue--;
+      res.render("result");
+    }
+  })
 });
+
+router.get('/set', function(req, res, next) {
+  var key = req.query.key;
+  if(key == secret.key) {
+    global.openValue = 1;
+    res.send('openvalue:'+global.openValue);
+  }else{
+    res.send('key is not avalible');
+  }
+
+});
+router.get('/get', function(req, res, next) {
+  res.send('ohhhhh:'+global.openValue);
+});
+
 
 module.exports = router;
